c680fb003523606df7069a59adfd8e5b41d21a9a
Luisa Novaes <luisanovaes.arq@gmail.com>
2022-01-25 18:12:18 -0300

    Add files via upload
    First project using React

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>January</h1>
+            <p>this is a Paragraph</p>
+        </div>
+        
+    )
+}
+
+export default Note;
